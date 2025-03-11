32e253b3fa19e6675960c35e35f193d6d3c51bcc
JXviews <37692072+taojiaxiangJs@users.noreply.github.com>
2025-03-11 09:18:23 +0800

    feat: 提交一次

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,21 @@ const activeKey = ref('1')
 const activityData = reactive({})
 const deliveryData = reactive({})
 
-const countActivityFee = (amount)=> {
-  activityData[]
+const countActivityFee = (amount, type)=> {
+  activityData[type].forEach(e=> {
+
+  })
 }
 
 const type = ref('')
 const amount = ref('30')
 const count = (type)=> {
   type.value = type
-  let activityList = countActivityFee(amount.value)
+  let activityList = countActivityFee(amount.value, type)
+  console.log(activityList);
 }
 const logFn = () => {
-  console.log(activityData, deliveryData);
+  console.log(JSON.parse(JSON.stringify(activityData)),JSON.parse(JSON.stringify(deliveryData)));
 }
 </script>
 
@@ -30,6 +33,12 @@ const logFn = () => {
     <a-button type="primary" size="small" @click="count('mt')">美团试算</a-button>
     <a-button type="primary" size="small" @click="count('ele')">饿了么试算</a-button>
   </a-space>
+  <div p-4></div>
+  <a-collapse v-model:activeKey="activeKey" :bordered="false" style="background: rgb(255, 255, 255)">
+    <a-collapse-panel key="3" header="基础商品" style="background: #f7f7f7; border-radius: 4px; margin-bottom: 24px; border: 0; overflow: hidden">
+      <foodsComponent></foodsComponent>
+    </a-collapse-panel>
+  </a-collapse>
   <a-collapse v-model:activeKey="activeKey" :bordered="false" style="background: rgb(255, 255, 255)">
     <a-collapse-panel key="1" header="我的活动" style="background: #f7f7f7; border-radius: 4px; margin-bottom: 24px; border: 0; overflow: hidden">
       <activityComponent v-model="activityData"></activityComponent>
@@ -40,12 +49,6 @@ const logFn = () => {
       <deliveryComponent v-model="deliveryData"></deliveryComponent>
     </a-collapse-panel>
   </a-collapse>
-  <a-collapse v-model:activeKey="activeKey" :bordered="false" style="background: rgb(255, 255, 255)">
-    <a-collapse-panel key="3" header="商品配置" style="background: #f7f7f7; border-radius: 4px; margin-bottom: 24px; border: 0; overflow: hidden">
-      <foodsComponent></foodsComponent>
-    </a-collapse-panel>
-  </a-collapse>
-  
 </template>
 
 <style scoped lang="scss"></style>
