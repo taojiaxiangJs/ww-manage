33c46f715f008a737a9c5560d49d8ba4b38b9df3
zt-2015 <zt-2015@zt-2015deMacBook-Pro.local>
2025-03-29 20:47:54 +0800

    feat:打包

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+console.log(process.env.NODE_ENV);
+// development  production
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),UnoCSS()],
+  base: process.env.NODE_ENV === 'development' ? './' : '/takeout',
+  plugins: [vue(), UnoCSS()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // 确保 @ 指向 src 目录
